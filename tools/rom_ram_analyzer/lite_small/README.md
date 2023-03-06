e27fe41685b2f906ee120e1cc1a8b9cdb1675e4c
aodongbiao <aodongbiao@huawei.com>
2023-03-06 15:40:21 +0800

    change folder name of rom_ram_analzyer
    Signed-off-by: aodongbiao <aodongbiao@huawei.com>

---
+++ tools/rom_ram_analyzer/lite_small/README.md
@@ -0,0 +1,47 @@
+# rom_ram_analyzer
+
+## 目的
+
+分析各部件的rom占用,结果以xls和json格式进行保存
+
+## 支持产品
+
+目标是支持所有的产品,但是目前由于配置文件没设计好,只支持:ipcamera_hispark_taurus ipcamera_hispark_taurus_linux wifiiot_hispark_pegasus
+
+## 代码思路
+
+1. 扫描BUILD.gn文件,收集各个target的编译产物及其对应的component_name, subsystem_name信息,并存储到config.yaml中的gn_info_file字段指定的json文件中
+2. 根据配置文件config.yaml扫描产品的编译产物目录,得到真实的编译产物信息(主要是大小)
+3. 用真实的编译产物与从BUILD.gn中收集的信息进行匹配,从而得到编译产物-大小-所属部件的对应信息
+4. 如果匹配失败,会直接利用grep到项目路径下进行搜索
+5. 如果还搜索失败,则将其归属到others
+
+
+## 使用
+
+前置条件：
+
+1. 获取整个本文件所在的整个目录
+1. 对系统进行编译
+1. linux平台
+1. python3.8及以后
+1. 安装requirements
+    ```txt
+    xlwt==1.3.0
+    ```
+
+1. `python3 rom_analysis.py --product_name {your_product_name} --oh_path {root_path_of_oh} [--recollect_gn bool]`运行代码,其中recollect_gn表示是需要重新扫描BUILD.gn还是直接使用已有结果.eg: `python3 rom_analysis.py --product_name ipcamera_hispark_taurus`
+3. 运行完毕会产生4个json文件及一个xls文件,如果是默认配置,各文件描述如下:
+   - gn_info.json:BUILD.gn的分析结果
+   - sub_com_info.json:从bundle.json中进行分析获得的各部件及其对应根目录的信息
+   - {product_name}_product.json:该产品实际的编译产物信息,根据config.yaml进行收集
+   - {product_name}_result.json:各部件的rom大小分析结果
+   - {product_name}_result.xls:各部件的rom大小分析结果
+
+## 新增template
+
+主要是在config.py中配置Processor,并在config.yaml中添加相应内容
+
+## 后续工作
+
+1. 部分log的输出有待优化
---
+++ tools/rom_ram_analyzer/lite_small/src/misc.py
@@ -0,0 +1,228 @@
+import logging
+import copy
+import os
+import logging
+from abc import ABC, abstractmethod
+from collections import defaultdict
+from typing import *
+import preprocess
+from pkgs.gn_common_tool import GnVariableParser
+from pkgs.simple_yaml_tool import SimpleYamlTool
+from pkgs.basic_tool import BasicTool
+
+
+_config = SimpleYamlTool.read_yaml("config.yaml")
+"""
+===============info handlers===============
+"""
+
+
+def extension_handler(paragraph: Text):
+    return GnVariableParser.string_parser("output_extension", paragraph).strip('"')
+
+
+def hap_name_handler(paragraph: Text):
+    return GnVariableParser.string_parser("hap_name", paragraph).strip('"')
+
+
+def target_type_handler(paragraph: Text):
+    tt = GnVariableParser.string_parser("target_type", paragraph).strip('"')
+    if not tt:
+        logging.info("parse 'target_type' failed, maybe it's a variable")
+    return tt
+
+
+"""
+===============gn lineno collector===============
+"""
+
+
+def gn_lineno_collect(match_pattern: str, project_path: str) -> DefaultDict[str, List[int]]:
+    """
+    在整个项目路径下搜索有特定target类型的BUILD.gn
+    :param match_pattern: 进行grep的pattern，支持扩展的正则
+    :param project_path: 项目路径（搜索路径）
+    :return: {gn_file: [line_no_1, line_no_2, ..]}
+    """
+    black_list = map(lambda x: os.path.join(
+        project_path, x), _config.get("black_list"))
+
+    def handler(content: Text) -> List[str]:
+        return list(filter(lambda y: len(y) > 0, list(map(lambda x: x.strip(), content.split("\n")))))
+
+    grep_list = BasicTool.grep_ern(match_pattern, path=project_path, include="BUILD.gn", exclude=tuple(black_list),
+                                   post_handler=handler)
+    gn_line_dict: DefaultDict[str, List[int]] = defaultdict(list)
+    for gl in grep_list:
+        gn_file, line_no, _ = gl.split(":")
+        gn_line_dict[gn_file].append(line_no)
+    return gn_line_dict
+
+
+"""
+===============target name parser===============
+"""
+
+
+class TargetNameParser:
+    @classmethod
+    def single_parser(cls, paragraph: Text) -> str:
+        """
+        查找类似shared_library("xxx")这种括号内只有一个参数的target的名称
+        :param paragraph: 要解析的段落
+        :return: target名称，如果是变量，不会对其进行解析
+        """
+        return BasicTool.re_group_1(paragraph, r"\w+\((.*)\)")
+
+    @classmethod
+    def second_parser(cls, paragraph: Text) -> str:
+        """
+        查找类似target("shared_library","xxx")这种的target名称（括号内第二个参数）
+        :param paragraph: 要解析的段落
+        :return: target名称，如果是变量，不会的其进行解析
+        """
+        return BasicTool.re_group_1(paragraph, r"\w+\(.*?, *(.*?)\)")
+
+
+"""
+===============post handlers===============
+"""
+
+
+class BasePostHandler(ABC):
+    @abstractmethod
+    def run(self, unit: Dict[str, AnyStr]) -> str:
+        ...
+
+    def __call__(self, unit: Dict[str, AnyStr]) -> str:
+        return self.run(unit)
+
+
+class DefaultPostHandler(BasePostHandler):
+    def run(self, unit: Dict[str, AnyStr]):
+        return unit["output_name"]
+
+
+class HAPPostHandler(BasePostHandler):
+    """
+    for ohos_hap"""
+
+    def run(self, unit: Dict[str, AnyStr]):
+        extension = _config.get("default_extension").get("app")
+        gn_hap_name = unit.get("hap_name")
+        if gn_hap_name:
+            return gn_hap_name+extension
+        return unit["output_name"]+extension
+
+
+class SOPostHandler(BasePostHandler):
+    """
+    for shared_library"""
+
+    def run(self, unit: Dict[str, AnyStr]):
+        output_name = unit["output_name"]
+        prefix = _config.get("default_prefix").get("shared_library")
+        if unit.get("extension"):
+            extension = unit.get("extension")
+        else:
+            extension = _config.get("default_extension").get("shared_library")
+        if not extension.startswith('.'):
+            extension = '.'+extension
+        if output_name.startswith(prefix):
+            return output_name+extension
+        return prefix+output_name+extension
+
+
+class APostHandler(BasePostHandler):
+    """
+    for static library"""
+
+    def run(self, unit: Dict[str, AnyStr]):
+        output_name = unit["output_name"]
+        prefix = _config.get("default_prefix").get("static_library")
+        extension: str = _config.get("default_extension").get("static_library")
+        if not extension.startswith('.'):
+            extension = '.'+extension
+        if output_name.startswith(prefix):
+            return output_name+extension
+        return prefix+output_name+extension
+
+
+class LiteLibPostHandler(BasePostHandler):
+    """
+    for lite_library"""
+
+    def run(self, unit: Dict[str, AnyStr]):
+        tp = unit["real_target_type"]
+        output_name = unit["output_name"]
+        if tp == "static_library":
+            prefix = _config.get("default_prefix").get("static_library")
+            extension = _config.get("default_extension").get("static_library")
+        elif tp == "shared_library":
+            prefix = _config.get("default_prefix").get("shared_library")
+            extension = _config.get("default_extension").get("shared_library")
+        else:
+            prefix = str()
+            extension = str()
+        if not extension.startswith('.'):
+            extension = '.'+extension
+        if output_name.startswith(prefix):
+            return output_name+extension
+        return prefix+output_name+extension
+
+
+class LiteComponentPostHandler(BasePostHandler):
+    """
+    for lite_component"""
+
+    def run(self, unit: Dict[str, AnyStr]):
+        tp = unit["real_target_type"]
+        output_name = unit["output_name"]
+        extension = unit.get("output_extension")
+        if tp == "shared_library":
+            prefix = _config.get("default_prefix").get("shared_library")
+            extension = _config.get("default_extension").get("shared_library")
+        else:
+            if tp != "executable":
+                unit["description"] = "virtual node"
+            prefix = str()
+            extension = str()
+        if not extension.startswith('.'):
+            extension = '.'+extension
+        return prefix+output_name+extension
+
+
+class TargetPostHandler(BasePostHandler):
+    """
+    for target(a,b){}"""
+
+    def run(self, unit: Dict[str, AnyStr]):
+        ...
+
+
+def LiteLibS2MPostHandler(unit: Dict, result_dict: Dict) -> None:
+    rt = unit.get("real_target_type")
+    new_unit = copy.deepcopy(unit)
+    if rt == "shared_library":
+        new_unit["real_target_type"] = "static_library"
+        k = LiteLibPostHandler()(new_unit)
+        new_unit["description"] = "may not exist"
+        result_dict["lite_library"][k] = new_unit
+    elif rt == "static_library":
+        new_unit["real_target_type"] = "shared_library"
+        k = LiteLibPostHandler()(new_unit)
+        new_unit["description"] = "may not exist"
+        result_dict["lite_library"][k] = new_unit
+    else:
+        logging.warning(
+            f"target type should be 'shared_library' or 'static_library', but got '{rt}'")
+        new_unit["real_target_type"] = "shared_library"
+        k = LiteLibPostHandler()(new_unit)
+        new_unit["description"] = "may not exist"
+        result_dict["lite_library"][k] = new_unit
+
+        new_new_unit = copy.deepcopy(unit)
+        new_new_unit["real_target_type"] = "static_library"
+        k = LiteLibPostHandler()(new_new_unit)
+        new_new_unit["description"] = "may not exist"
+        result_dict["lite_library"][k] = new_new_unit
