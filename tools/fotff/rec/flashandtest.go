82d97f3af10e4e8d8970c115b88fc47cd891aa22
mujingrong <moujingrong@huawei.com>
2023-04-20 17:35:33 +0800

    refactor test command
    Signed-off-by: mujingrong <moujingrong@huawei.com>

--- tools/fotff/rec/flashandtest.go
+++ tools/fotff/rec/flashandtest.go
@@ -19,48 +19,46 @@ type FlashAndTestOptions struct {
 
 // FlashAndTest build and flash the given version to the specified device, then run the specified test cases
 func FlashAndTest(ctx context.Context, opt *FlashAndTestOptions) error {
-	result, err := doFlashAndTest(ctx, opt)
-	if err != nil {
-		return err
-	}
-	logrus.Infof("Flash and test for version %s and test case %s done, result: %s", opt.Version, opt.TestCase, result)
-	return nil
-}
-
-func doFlashAndTest(ctx context.Context, opt *FlashAndTestOptions) (tester.ResultStatus, error) {
 	// flash the specified version to the specified device
 	if err := opt.M.Flash(opt.Device, opt.Version, ctx); err != nil {
 		logrus.Errorf("Failed to flash version %s to device %s, error: %s", opt.Version, opt.Device, err.Error())
-		return tester.ResultFail, err
+		return err
 	}
+
 	// prepare and run the specified test
 	if err := opt.T.Prepare(opt.M.PkgDir(opt.Version), opt.Device, ctx); err != nil {
 		logrus.Errorf("Failed to prepare test, error: %s", err.Error())
-		return tester.ResultFail, err
+		return err
 	}
+
 	if opt.TestCase == "" {
 		// run all test cases if the --testcase argument was not present
 		results, err := opt.T.DoTestTask(opt.Device, ctx)
 		if err != nil {
 			logrus.Errorf("Failed to run all test cases on device %s, error: %s", opt.Device, err.Error())
-			return tester.ResultFail, err
+			return err
 		}
+
 		// only mark test result as pass when all test cases passed
-		var ret tester.ResultStatus = tester.ResultPass
+		var result tester.ResultStatus = tester.ResultPass
 		for _, r := range results {
 			logrus.Infof("Result for test case %s is %s", r.TestCaseName, r.Status)
 			if r.Status == tester.ResultFail {
-				ret = tester.ResultFail
+				result = tester.ResultFail
 			}
 		}
-		return ret, nil
+
+		logrus.Infof("Flash and test for all test cases done, result: %s", result)
+		return nil
 	} else {
 		// otherwise run the specified test case
 		result, err := opt.T.DoTestCase(opt.Device, opt.TestCase, ctx)
 		if err != nil {
 			logrus.Errorf("Failed to run test case %s on device %s, error: %s", opt.TestCase, opt.Device, err.Error())
-			return tester.ResultFail, err
+			return err
 		}
-		return result.Status, nil
+
+		logrus.Infof("Flash and test for test case %s done, result: %s", opt.TestCase, result)
+		return nil
 	}
 }
