f259db06bfd98ee7b96ff4c847df28bf353092fa
贾孟元 <jiamengyuan@h-partners.com>
2024-08-01 02:17:08 +0000

    update cases/smoke/basic/screenshot32/new_script/testcases/test_camera.py. 照相机录像停止调整
    Signed-off-by: 贾孟元 <jiamengyuan@h-partners.com>

--- cases/smoke/basic/screenshot32/new_script/testcases/test_camera.py
+++ cases/smoke/basic/screenshot32/new_script/testcases/test_camera.py
@@ -32,12 +32,8 @@ class Test:
         device.save_snapshot_to_local('{}_camera_step4.jpeg'.format(device.sn))
 
         logging.info('stop recoding')
-        device.click(320, 1095)
-        time.sleep(2)
-        device.click(320, 1095)
-        time.sleep(2)
-        device.click(320, 1095)
-        time.sleep(4)
+        device.hdc_shell(f'uitest uiInput click 320 1095')
+        time.sleep(3)
         device.save_snapshot_to_local('{}_camera_step5.jpeg'.format(device.sn))
 
         logging.info('click left behind button in order to switch to gallery app')
