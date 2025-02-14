cf47f695b176b6076ee75cf3183dc018efcb3d40
oh_ci <maguangsheng1@huawei.com>
2025-02-14 01:27:45 +0000

    回退 'Pull Request !593 : 【轻量级 PR】：update cases/smoke/basic/screenshot32/new_script/testcases/test_settings_wifi.py.'

--- cases/smoke/basic/screenshot32/new_script/testcases/test_settings_wifi.py
+++ cases/smoke/basic/screenshot32/new_script/testcases/test_settings_wifi.py
@@ -32,8 +32,8 @@ class Test:
         logging.info('turn on/off wlan swith')
         device.click(646, 210)
         #device.hdc_shell(f'uitest uiInput click 646 210')
-        time.sleep(12)
+        time.sleep(15)
         device.save_snapshot_to_local('{}_after_click.jpeg'.format(device.sn))
         after_click = device.get_wifi_status().get('active')
         logging.info('wlan switch changes from [{}] to [{}]'.format(before_click, after_click))
-        #assert before_click != after_click, 'wlan switch turn on/off failed'
+        assert before_click != after_click, 'wlan switch turn on/off failed'
