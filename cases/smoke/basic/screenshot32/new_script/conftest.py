6f266f966ce8c0bf4efeab213c6c26d9db429bae
oh_ci <maguangsheng1@huawei.com>
2025-02-19 01:34:01 +0000

    回退 'Pull Request !630 : 【轻量级 PR】：update cases/smoke/basic/screenshot32/new_script/conftest.py.'

--- cases/smoke/basic/screenshot32/new_script/conftest.py
+++ cases/smoke/basic/screenshot32/new_script/conftest.py
@@ -39,8 +39,6 @@ def setup_teardown(request, device):
     time.sleep(2)
     device.go_home()
     time.sleep(1)
-    device.click(595, 555)
-    time.sleep(10)
     if device.get_focus_window() == 'SystemDialog1':
         device.click(595, 555)
     time.sleep(15)
