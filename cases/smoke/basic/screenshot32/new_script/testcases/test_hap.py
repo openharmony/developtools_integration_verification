2d9ab8a6e3efa92c93a1d166149b95ea05750a78
马广生 <maguangsheng1@h-partners.com>
2025-07-04 07:46:11 +0000

    add helloworld测试
    Signed-off-by: 马广生 <maguangsheng1@h-partners.com>

---
+++ cases/smoke/basic/screenshot32/new_script/testcases/test_hap.py
@@ -0,0 +1,37 @@
+import logging
+import os.path
+import time
+
+import pytest
+
+from utils.images import compare_image_similarity, crop_picture
+
+
+@pytest.fixture(autouse=True)
+def uninstall_hap(device):
+    yield
+    try:
+        device.uninstall_hap('com.example.helloworld')
+    except:
+        pass
+
+
+class Test:
+    ability_name = 'EntryAbility'
+    bundle_name = 'com.example.helloworld'
+
+    @pytest.mark.parametrize('setup_teardown', [None], indirect=True)
+    def test(self, setup_teardown, device):
+        logging.info('install hap')
+        hap_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(device.resource_path))), 'entry-default-signed.hap')
+        device.install_hap(hap_path)
+        time.sleep(1)
+        logging.info('start app')
+        device.start_ability(self.bundle_name, self.ability_name)
+        time.sleep(2)
+        main_page = device.save_snapshot_to_local('{}_hap.jpeg'.format(device.sn))
+        crop_picture(picture=main_page, x1=0, y1=72, x2=297, y2=290)
+        stand_pic = os.path.join(device.resource_path, 'hap.jpeg')
+
+        similarity = compare_image_similarity(main_page, stand_pic)
+        assert similarity > 0.8, '截图对比失败'
