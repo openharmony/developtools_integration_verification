416e25f510f31ccf3f7d76c8f7ded9e674a84d14
hongjianxiaozi <hanxingxing1@h-partners.com>
2025-10-20 11:05:52 +0800

    upload: 上传文件
    Signed-off-by: hongjianxiaozi <hanxingxing1@h-partners.com>

---
+++ cases/smoke/basic/screenshot32/new_script/testcases/meituan_hap.py
@@ -0,0 +1,35 @@
+import logging
+import os.path
+import time
+
+import pytest
+
+from utils.images import compare_image_similarity, crop_picture
+
+
+@pytest.fixture(autouse=True)
+def uninstall_hap(device):
+    yield
+    try:
+        device.uninstall_hap('com.example.meituan')
+    except:
+        pass
+
+
+class Test:
+    ability_name = 'EntryAbility'
+    bundle_name = 'com.example.meituan'
+
+    @pytest.mark.parametrize('setup_teardown', [None], indirect=True)
+    def test(self, setup_teardown, device):
+        logging.info('install hap')
+        hap_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(device.resource_path))), 'fangmeituan','entry-default-signed.hap')
+        device.install_hap(hap_path)
+        time.sleep(10)
+        logging.info('start app')
+        device.start_ability(self.bundle_name, self.ability_name)
+        time.sleep(10)
+        main_page = device.save_snapshot_to_local('{}_hap.jpeg'.format(device.sn))
+        stand_pic = os.path.join(device.resource_path, 'meituan_hap.jpeg')
+        similarity = compare_image_similarity(main_page, stand_pic)
+        assert similarity > 0.8, '截图对比失败'
