e837c02a9cac78e73375a7302724b2e45a2acab0
hongjianxiaozi <hanxingxing1@h-partners.com>
2025-11-14 09:01:56 +0800

    update: 更新文件 check.py
    Signed-off-by: hongjianxiaozi <hanxingxing1@h-partners.com>

--- tools/check.py
+++ tools/check.py
@@ -19,20 +19,26 @@ for project in root.findall("project"):
             matched_paths.append(matched_path)
 
 check_list = ['system/app/ArkWebCore','system/app/ArkWebCoreLegacy','ArkWebCore.hap','ArkWebCoreLegacy.hap','com.huawei.hmos.arkwebcore','com.huawei.hmos.arkwebcorelegacy','ArkWebCore','ArkWebCoreLegacy']
-def check_paths_in_files(file_paths):
+def check_paths_in_files(file_paths,check_list):
     result = 0
     for file_path in file_paths:
         if "base/web/webview" in file_path:
-            with open(file_path, 'r',encoding='utf-8',errors='ignore') as file:
-                for line in file:
-                    for item in check_list:
-                        if 'system/app/ArkWebCore' in line:
-                            print(f"文件 {file_path} 中含有违规字段{item}，请修改,代码中不能含有{check_list}")
-                            result = 1
+            if os.path.exists(file_path):
+                with open(file_path, 'r',encoding='utf-8',errors='ignore') as file:
+                    check_result = False
+                    for line in file:
+                        for item in check_list:
+                            if item in line:
+                                if not check_result:
+                                    print(f"文件 {file_path} 中含有违规字段{item}，请修改,代码中不能含有{check_list}")
+                                    check_result = True
+                                    result = 1
+                                break
+                        if check_result:
                             break
     return result
 
-result = check_paths_in_files(matched_paths)
+result = check_paths_in_files(matched_paths,check_list)
 print(f"检查结果: {result}")
 exit(1 if result else 0)
     
