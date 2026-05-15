5e5aa12d09fe10263bf13084834aba1242cbd141
xiang-kai-chen <chenxiangkai1@huawei.com>
2026-05-15 15:41:17 +0800

    调整联系人、备忘录的冒烟用例，解决用例失败问题
    Signed-off-by: xiang-kai-chen <chenxiangkai1@huawei.com>

--- cases/smoke/basic/screenshotP7885/new_script/testcases/test_note.py
+++ cases/smoke/basic/screenshotP7885/new_script/testcases/test_note.py
@@ -17,13 +17,9 @@ class Test:
         # 截取笔记主页面
         device.save_snapshot_to_local('{}_note_mainpage.jpeg'.format(device.sn))
 
-        # 点击数学公式
-        logging.info('click shuxue gongshi')
-        device.refresh_layout()
-        math_formula_element = device.get_element_by_text('数学公式')
-        assert math_formula_element, '数学公式 component not found'
-        device.click_element(math_formula_element)
-
+        # 点击左侧笔记进入内容页
+        logging.info('click note bookmark')
+        device.click(242, 367)
         time.sleep(4)
         logging.info('click note content area')
         device.refresh_layout()
