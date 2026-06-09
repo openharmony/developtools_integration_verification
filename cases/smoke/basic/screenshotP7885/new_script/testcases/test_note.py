0740d098d9fe42d2ca29034f94609d8a6f2e3aa2
xiang-kai-chen <chenxiangkai1@huawei.com>
2026-06-09 10:53:23 +0800

    将7885冒烟测试用例合入 LTS，仅包括开机启动等基础测试用例，不包括系统应用用例
    Signed-off-by: xiang-kai-chen <chenxiangkai1@huawei.com>

---
+++ cases/smoke/basic/screenshotP7885/new_script/testcases/test_note.py
@@ -0,0 +1,38 @@
+import logging
+import os.path
+import time
+
+import pytest
+from utils.images import compare_image_similarity, crop_picture
+
+
+class Test:
+    ability_name = 'MainAbility'
+    bundle_name = 'com.ohos.note'
+
+    @pytest.mark.parametrize('setup_teardown', [bundle_name], indirect=True)
+    def test(self, setup_teardown, device):
+        logging.info('start note app')
+        device.start_ability(self.bundle_name, self.ability_name)
+        # 截取笔记主页面
+        device.save_snapshot_to_local('{}_note_mainpage.jpeg'.format(device.sn))
+
+        # 点击左侧笔记进入内容页
+        logging.info('click note bookmark')
+        device.click(242, 367)
+        time.sleep(4)
+        logging.info('click note content area')
+        device.refresh_layout()
+        for i in range(5):
+            device.click(360, 325)
+            time.sleep(2)
+            device.click(360, 324)
+            time.sleep(3)
+            if device.is_soft_keyboard_on():
+                break
+        time.sleep(3)
+        standard_pic = os.path.join(device.resource_path, 'note.jpeg')
+        note_pic = device.save_snapshot_to_local('{}_note.jpeg'.format(device.sn))
+        logging.info('compare image similarity')
+        similarity = compare_image_similarity(note_pic, standard_pic)
+        assert similarity > 0.5, 'compare similarity failed'
